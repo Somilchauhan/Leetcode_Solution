d238c4fa23eb9d24a9d33e6a0416627141c37b79
Somilchauhan <somilkhan755@gmail.com>
2025-08-06 13:07:28 +0530

    Added a new problem of infix to prefix using stack

--- Delete Middle Element of Linked List/ListNode.java
+++ Delete Middle Element of Linked List/ListNode.java
@@ -8,6 +8,15 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+ public class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+
 class Solution {
     public ListNode deleteMiddle(ListNode head) {
         ListNode slow = head;
