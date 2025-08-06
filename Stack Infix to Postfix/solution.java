6cc50eb34edb8b6f6592a399bb4950619ba435f8
Somilchauhan <somilkhan755@gmail.com>
2025-08-06 13:13:33 +0530

    Added a new code for convert the string from infix to postfix using stack

---
+++ Stack Infix to Postfix/solution.java
@@ -0,0 +1,72 @@
+import java.util.Stack;
+
+public class solution {
+
+    static String convertToPostfix(String s) {
+        Stack<Character> op = new Stack<>();
+        Stack<String> val = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            
+            if (c == ' ') {
+                continue;
+            }
+            
+            if (Character.isDigit(c)) {
+                int num = 0;
+                while (i < s.length() && Character.isDigit(s.charAt(i))) {
+                    num = num * 10 + (s.charAt(i) - '0');
+                    i++;
+                }
+                i--;
+                val.push(num + "");
+            } 
+            else if (c == '(') {
+                op.push(c);
+            } 
+            else if (c == ')') {
+                while (!op.isEmpty() && op.peek() != '(') {
+                    String val2 = val.pop();
+                    String val1 = val.pop();
+                    val.push(val1 + val2 + op.pop());
+                }
+                op.pop(); // remove '('
+            } 
+            else if (isOperator(c)) {
+                while (!op.isEmpty() && hasPrecedence(c, op.peek())) {
+                    String val2 = val.pop();
+                    String val1 = val.pop();
+                    val.push(val1 + val2 + op.pop());
+                }
+                op.push(c);
+            }
+        }
+
+        while (!op.isEmpty()) {
+            String val2 = val.pop();
+            String val1 = val.pop();
+            val.push(val1 + val2 + op.pop());
+        }
+
+        return val.pop();
+    }
+
+    private static boolean isOperator(char c) {
+        return c == '+' || c == '-' || c == '*' || c == '/';
+    }
+
+    private static boolean hasPrecedence(char op1, char op2) {
+        if (op2 == '(' || op2 == ')') {
+            return false;
+        }
+        return !((op1 == '*' || op1 == '/') && (op2 == '+' || op2 == '-'));
+    }
+
+    public static void main(String[] args) {
+        String s = "6+(3-2)*6/2";
+        System.out.println("The Infix String is: " + s);
+        String res = convertToPostfix(s);
+        System.out.println("The solution of this expression is: " + res);
+    }
+}
